b7f0bf1de064133014532c86f6db4c45ca90f6b1
kim-macbook <creator@hanyang.ac.kr>
2020-08-04 18:12:38 +0900

    Edit TodoScreen

--- app/screens/TodoScreen.tsx
+++ app/screens/TodoScreen.tsx
@@ -1,51 +1,16 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
 import { CustomStackScreenProp } from './types';
 import styles from './styles';
-import Input from '../components/common/Input';
-import useInput from '../hooks/common/useInput';
 
 function TodoScreen({
   route: {
     params: { data },
   },
 }: CustomStackScreenProp<'Todo'>) {
-  const { hardenForm, onChangeText } = useInput();
-  const { todo } = hardenForm;
-  useEffect(() => {
-    onChangeText('todo', 'title')(data.title);
-    onChangeText('todo', 'amount')(String(data.amount));
-    onChangeText('todo', 'unit')(data.unit);
-    onChangeText('todo', 'startTime')(data.startTime);
-    onChangeText('todo', 'endTime')(data.endTime);
-  }, []);
   return (
     <SafeAreaView style={styles.safeAreaViewContainer}>
-      <Input
-        title="Title"
-        value={todo.title}
-        onChangeText={onChangeText('todo', 'title')}
-      />
-      <Input
-        title="amount"
-        value={todo.amount}
-        onChangeText={onChangeText('todo', 'amount')}
-      />
-      <Input
-        title="unit"
-        value={todo.unit}
-        onChangeText={onChangeText('todo', 'unit')}
-      />
-      <Input
-        title="start Time"
-        value={todo.startTime}
-        onChangeText={onChangeText('todo', 'startTime')}
-      />
-      <Input
-        title="end Time"
-        value={todo.endTime}
-        onChangeText={onChangeText('todo', 'endTime')}
-      />
+      <Text>{data.title}</Text>
     </SafeAreaView>
   );
 }
